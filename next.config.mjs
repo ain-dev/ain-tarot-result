fa2b81290ea1550ec4b5eefa205e6f6999c537b3
kangzwon <kkgi1234@gendata.kr>
2024-08-01 15:48:01 +0900

    fix: next.config.mjs > 외부 이미지 허용 케이스 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,12 @@ const nextConfig = {
     instrumentationHook: true,
   },
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ['image/avif', 'image/webp'],
     domains: [
-      "192.168.68.57",
-      "picsum.photos",
-      "5ae7b9223ead1b8df22cc8a8f0bd7579.r2.cloudflarestorage.com",
+      'picsum.photos',
+      '079ef30d9d4760b7b5ee02b920e58155.r2.cloudflarestorage.com',
+      '5ae7b9223ead1b8df22cc8a8f0bd7579.r2.cloudflarestorage.com',
+      'backend',
     ],
   },
   reactStrictMode: true,
